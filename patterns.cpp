403912cce84ed259be626f2d869076d0cd37f8fc
Mangesh andhale <mangeshandhale02@gmail.com>
2026-07-02 15:08:11 +0530

    Resolve merge conflict

--- patterns.cpp
+++ patterns.cpp
@@ -153,3 +153,22 @@ public:
         }
     }
 };
+
+
+// Palindrome Number
+class Solution {
+public:
+    bool isPalindrome(int n) {
+        int original = n;
+        int reverse = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+
+        return original == reverse;
+    }
+};
+
