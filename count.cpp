6e0fef708e31a30756b37586a1ce291f53fde3a1
Mangesh andhale <mangeshandhale02@gmail.com>
2026-07-02 16:47:23 +0530

    Resolve merge conflict

--- count.cpp
+++ count.cpp
@@ -105,3 +105,18 @@ public:
     }
 };
 
+// Check for Prime Number
+class Solution {
+public:
+    bool isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+};
