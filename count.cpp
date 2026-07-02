7b56b61ab4802f9929c9fd3f3044b7da579a645d
Mangesh andhale <mangeshandhale02@gmail.com>
2026-07-02 16:47:10 +0530

    Resolve merge conflict

--- count.cpp
+++ count.cpp
@@ -90,3 +90,18 @@ public:
     }
 };
 
+// Divisors of a Number
+class Solution {
+public:
+    vector<int> divisors(int n) {
+        vector<int> ans;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
+
