3092423ea6959569ecf80ee434028dbbfb018cfb
Mangesh andhale <mangeshandhale02@gmail.com>
2026-07-05 13:53:26 +0530

    Resolve merge conflict

--- Recursion.cpp
+++ Recursion.cpp
@@ -13,3 +13,19 @@ public:
         cout << n << endl;
     }
 };
+
+class Solution {
+public:
+    void printNos(int n) {
+        // Base case
+        if (n == 0) {
+            return;
+        }
+
+        // Print first
+        cout << n << endl;
+
+        // Recursive call
+        printNos(n - 1);
+    }
+};
