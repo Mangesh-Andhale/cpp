cb7d859e025bd101de3869640034d29c25c8b222
Mangesh andhale <mangeshandhale02@gmail.com>
2026-07-05 14:51:34 +0530

    Resolve merge conflict

--- Recursion.cpp
+++ Recursion.cpp
@@ -46,3 +46,18 @@ public:
         return n + sumOfFirstN(n - 1);
     }
 };
+	
+// Factorial of a given number
+
+class Solution {
+public:
+    int factorial(int n) {
+        // Base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        // Recursive case
+        return n * factorial(n - 1);
+    }
+};
